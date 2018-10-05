bfcdf7dde713dd7cbeada022a6bf4a855c57952e
DenisSRemo <43639388+DenisSRemo@users.noreply.github.com>
2018-10-05 16:00:21 +0100

    ex6

---
+++ ex6.py
@@ -0,0 +1,36 @@
+name=input("Put your name:")
+age=input("Put your age:")
+height=input("Put your height in cm:")
+weight=input("Put your weight in kg:")
+eyecolour=input("Put your eye colour:")
+haircolur=input("Put your hair colour")
+if(age<=3):
+    print("baby")
+    else
+    if(age>3 and age<14):
+        print("child")
+        else
+        if(age>=14 and age<=18):
+            print("teen")
+            else
+            if(age>18 and age<65):
+                print("adult")
+                else
+                if(age>=65):
+                    print("old,but wise")
+if(height<140):
+    print("small")
+    else
+    if(height>=140 and height<=180):
+        print("average")
+        else
+        if(height>180):
+            print("tall")
+if(weight<55):
+    print("light weight")
+    else
+    if(weight>=55 and weight<=80):
+        print("average")
+        else
+        if(weight>80):
+            print("big boned")
