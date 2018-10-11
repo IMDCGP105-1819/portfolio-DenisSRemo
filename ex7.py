f27ca68f9a6d893a831904a2ec1ce56b8ea35d60
DenisSRemo <43639388+DenisSRemo@users.noreply.github.com>
2018-10-11 10:57:39 +0100

    Update ex7.py

--- ex7.py
+++ ex7.py
@@ -4,10 +4,11 @@ total_cost=float(input("Enter the cost of your dream home:"))
 m=0
 portion_deposit=0.20
 current_savings=0
+m_salary=annual_salary/12
 r=0.04
 money_needed=total_cost*portion_deposit
 while(current_savings<money_needed):
-    current_savings=annual_salary/12*portion_saved
+    current_savings+=m_salary*portion_saved
     current_savings=+current_savings*r/12
     m+=1
 print(m)
