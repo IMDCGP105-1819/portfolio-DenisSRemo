089891fcaf3086448ce6bc9bc9de6d1d8aae932a
DenisSRemo <43639388+DenisSRemo@users.noreply.github.com>
2018-10-11 10:55:13 +0100

    Update ex7.py

--- ex7.py
+++ ex7.py
@@ -1,16 +1,13 @@
-print("Enter your annual salary:")
-annual_salary=float(input())
-print("Enter the percent of your salary to save, as a decimal: ")
-portion_saved=float(input())
-print("Enter the cost of your dream home:")
-total_cost=float(input())
+annual_salary=float(input("Enter your annual salary:"))
+portion_saved=float(input("Enter the percent of your salary to save, as a decimal: "))
+total_cost=float(input("Enter the cost of your dream home:"))
 m=0
-portion_deposit=total_cost/10*2
+portion_deposit=0.20
 current_savings=0
 r=0.04
-total_cost=total_cost-portion_deposit
-while(current_savings!=total_cost):
-    portion_saved=annual_salary/12/10
-    current_savings=current_savings+portion_saved+current_savings*r/12
-    m=m+1
-print("Number of months:"+m)
+money_needed=total_cost*portion_deposit
+while(current_savings<money_needed):
+    current_savings=annual_salary/12*portion_saved
+    current_savings=+current_savings*r/12
+    m+=1
+print(m)
