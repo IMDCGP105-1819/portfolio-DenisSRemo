eb238a26d27d0857a476e27c0b3c8e243776dfb9
DenisSRemo <43639388+DenisSRemo@users.noreply.github.com>
2018-10-18 11:24:05 +0100

    Add files via upload

--- ex10.py
+++ ex10.py
@@ -1,14 +1,14 @@
 def division(x):
     if(x%3==0 and x%5==0):
         print("FizzBuzz")
-        else:
-            if(x%3==0):
+    else:
+        if(x%3==0):
             print("Fizz")
+        else:
+            if(x%5==0):
+                print("Buzz")
             else:
-                if(x%5==0):
-                    print("Buzz")
-                    else:
-                        print(x)
+                print(x)
 high=int(input())
 low=int(input())
 for i in range(low,high):
