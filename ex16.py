4ecbe95dc911c79ea1240e0357c6e00274264999
DenisSRemo <43639388+DenisSRemo@users.noreply.github.com>
2018-11-15 11:16:50 +0000

    Add files via upload

---
+++ ex16.py
@@ -0,0 +1,13 @@
+class Fraction(object):
+    def__init(self,num,denom):
+        self.num=num;
+        self.denom=denom;
+def addition(self,other):
+    return((self.num*other.denom+self.denom*other.num)/(self.denom*other.denom))
+def substraction(self,other):
+    return((self.num*other.denom-self.denom*other.num)/(self.num*other.num))
+def division(self,other):
+    return((self.num*other.denom)/(self.denom*other.num))
+def inverse(self):
+    return(self.denom/self.num)
+
