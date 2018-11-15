2f2978f8556795f18406667225291d3cfba26ebc
DenisSRemo <43639388+DenisSRemo@users.noreply.github.com>
2018-11-15 11:30:51 +0000

    Add files via upload

---
+++ ex17.py
@@ -0,0 +1,11 @@
+class Car(object):
+    def__init__(self,colour,model,manufacturer,numdoors):
+        self.colour=colour;
+        self.model=model;
+        self.manufacturer=manufacturer;
+        self.numdoors=numdoors;
+self.colour=input()
+self.model=input()
+self.manufacturer=imput()
+self.numdoors=input()
+print(self.colour+","+self.model+","+self.manufacturer+","+self.numdoors)
